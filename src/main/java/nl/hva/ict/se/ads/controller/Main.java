d1c4d6f878ec27e3ec1033f97208c0c512af0d11
jvw <jvw91@msn.com>
2019-10-13 20:52:25 +0200

    Updated main, so i don't have de smash the run button all the time.

--- src/main/java/nl/hva/ict/se/ads/controller/Main.java
+++ src/main/java/nl/hva/ict/se/ads/controller/Main.java
@@ -6,12 +6,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number of archers: ...");
-        int numOfArchers = scanner.nextInt();
-        System.out.println("\n");
+        boolean run = true;
 
-        Runner.sSort(numOfArchers);
-        Runner.qSort(numOfArchers);
-        Runner.cSort(numOfArchers);
+        while (run) {
+            System.out.println("Enter number of archers: ...\n(0 to quit)");
+            int numOfArchers = scanner.nextInt();
+            if (numOfArchers == 0) {
+                System.out.println("GoodBye!");
+                run = false;
+            } else {
+                if (numOfArchers > 25000) {
+                    System.out.println("Woah! Much archers, Much Sorting....\n");
+                }
+
+                Runner.sSort(numOfArchers);
+                Runner.qSort(numOfArchers);
+                Runner.cSort(numOfArchers);
+            }
+        }
     }
 }
